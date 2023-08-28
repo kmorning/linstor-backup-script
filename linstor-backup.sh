d5dd2e3287acdc643fcebe92bc74c94fe66273a7
Kelly Morning <kmorning@utilitieskingston.com>
2023-08-28 12:03:45 -0400

    backup resource definitions

--- linstor-backup.sh
+++ linstor-backup.sh
@@ -86,6 +86,17 @@ linstor -m sp l | jq -jr '.[] | select(.stor_pools) | .stor_pools[] | select(.dr
   done
 
 echo
+echo "# Resource Groups"
+linstor -m resource-group list | jq -r '.[][] | "\(.name) \(.select_filter.place_count) \(.select_filter.storage_pool) \(try .select_filter.layer_stack | join(","))"' |
+  while read -r name place_count storage_pool layer_stack; do
+    echo "linstor resource-group create $name --storage-pool $storage_pool --place-count $place_count --layer-list $layer_stack"
+    linstor -m resource-group list-properties $name | jq -jr '.[][] | .key, " ", .value, "\n"' |
+      while read key value; do
+        echo "  linstor resource-group set-property $name $key $value"
+      done
+  done
+
+echo
 echo "# Resource Definitions"
 linstor -m resource-definition list | jq -jr '.[] | select(.rsc_dfns) | .rsc_dfns[] | .rsc_name, " ", .rsc_dfn_port, "\n"' |
   while read rsc_name rsc_dfn_port; do
