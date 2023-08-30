faaf8840f9937eeea9f6ca9b9d5cf4c09bdf2bde
Kelly Morning <kmorning@utilitieskingston.com>
2023-08-30 11:49:27 -0400

    fix resource definition name

--- test/resource-definition.sh
+++ test/resource-definition.sh
@@ -3,8 +3,8 @@ echo "# Resource Definitions"
 linstor -m --output-version v1 resource-definition list | jq -r '.[][] | "\(.name) \(.layer_data[0].data.port) \(.resource_group_name)"' |
   while read name port resource_group; do
     echo "linstor resource-definition create -p $port --resource-group $resource_group $name"
-    linstor -m resource-definition list-properties $rsc_name | jq -jr '.[][] | .key, " ", .value, "\n"' |
+    linstor -m resource-definition list-properties $name | jq -jr '.[][] | .key, " ", .value, "\n"' |
       while read key value; do
-        echo "  linstor resource-definition set-property $rsc_name $key $value"
+        echo "  linstor resource-definition set-property $name $key $value"
       done
   done
